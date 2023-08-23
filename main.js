58dc60eb3be2561b1c662721608e2f923de261a9
Miles-Collins <miles.collins@ymail.com>
2023-08-23 17:05:04 -0600

    sync

---
+++ main.js
@@ -0,0 +1,53 @@
+const animals = [
+  {
+    name: "Tony",
+    mood: "🥰",
+    emoji: "🐅",
+    hunger: 100,
+  },
+  {
+    name: "Harambe",
+    mood: "🥰",
+    emoji: "🦍",
+    hunger: 100,
+  },
+  {
+    name: "Otto",
+    mood: "🥰",
+    emoji: "🦦",
+    hunger: 100,
+  },
+  {
+    name: "Thux",
+    mood: "🥰",
+    emoji: "🐧",
+    hunger: 100,
+    habitat: "ice",
+  },
+  {
+    name: "Roth",
+    mood: "🥰",
+    emoji: "🦥",
+    hunger: 100,
+  },
+  {
+    name: "Ned",
+    mood: "🥰",
+    emoji: "🐙",
+    hunger: 100,
+    habitat: "water",
+  },
+  {
+    name: "Kevin",
+    mood: "🥰",
+    emoji: "🦚",
+    hunger: 100,
+  },
+  {
+    name: "Oslo",
+    mood: "🥰",
+    emoji: "🦧",
+    hunger: 100,
+    habitat: "library",
+  },
+];
